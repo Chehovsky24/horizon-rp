f0c85993c34cef548084c890fc3c8f9d05df9006
kwert1k887 <126343705+kwert1k887@users.noreply.github.com>
2024-02-20 21:47:39 +0300

    Добавлен спавн при регистрации и dimension теперь равен 0

--- sources/src/server/basic/auth.js
+++ sources/src/server/basic/auth.js
@@ -1,5 +1,6 @@
 mp.events.add('playerReady', (player) => {
     player.call('auth');
+    player.dimension = 0;
 });
 
 mp.events.add('sendDataToAuthorization', (player, username, pass) => {
@@ -33,6 +34,8 @@ mp.events.add('sendDataToRegister', (player, email, loginReg, pass1, promo) => {
                         console.log('Ошибка при регистрации:', err);
                     } else {
                         console.log('Пользователь успешно зарегистрирован.');
+                        player.position = new mp.Vector3(-1036.72, -2732.11, 13.76);
+                        player.heading = 326.25;
                         player.call('Ready');
                     }
                 });
