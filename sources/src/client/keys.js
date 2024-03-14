505235bdbcdbcaa6b8c9d67b0f3a67d3c7fa00a8
kwert1k887 <126343705+kwert1k887@users.noreply.github.com>
2024-03-14 13:07:58 +0300

    Добавил возможность показывать и скрывать худ

--- sources/src/client/keys.js
+++ sources/src/client/keys.js
@@ -1,3 +1,15 @@
 mp.keys.bind(192, false, () => {
     mp.gui.cursor.visible = !mp.gui.cursor.visible;
 });
+
+var hudHidden = false;
+
+mp.keys.bind(48, false, () => {
+    if (hudHidden) {
+        callCef('hud', '{"type": "show"}');
+        hudHidden = false;
+    } else {
+        callCef('hud', '{"type": "hide"}');
+        hudHidden = true;
+    }
+});
